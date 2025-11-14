3a47192f3ef2fdc35096908f56e648cd7f3003b1
vishnuanil_frgr <vishnuanil@qburst.com>
2025-11-15 03:29:13 +0530

    fixed role in /post user

--- app/src/controllers/usersController.js
+++ app/src/controllers/usersController.js
@@ -57,7 +57,7 @@ export const getUserById = (req, res) => {
   const user = users.find(u => u.id === Number(id));
 
   if (!user) {
-    return res.status(404).json({ error: "User not found" });
+    return res.status(404).json({ error: "User not found " });
   }
 
   return res.status(200).json(user);
